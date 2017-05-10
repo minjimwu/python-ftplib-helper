2909fa26c4afdc1b2c129f6bea832c3221fe9f50
jim wu <jim_wu@trend.com.tw>
2017-05-10 17:35:27 +0800

    add for pip install

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+   name='python-ftplib-helper',
+   version='1.0',
+   description='helper for ftplib',
+   author='Jim Wu',
+   author_email='minjimwu@gmail.com',
+   packages=['python-ftplib-helper'],
+   install_requires=[],
+   scripts=[]
+)
